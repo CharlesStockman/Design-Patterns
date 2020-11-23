01d023540ed3f861a3ba47a51e36cccfe10a45ec
Charles Stockman <chuck.stockman@yahoo.com>
2020-11-23 02:59:13 -0500

    Added the ability to test the happy path for CodeBuilder.addField()

--- src/test/java/org/charlesStockman/designPatterns/creation/AdapterTest.java
+++ src/test/java/org/charlesStockman/designPatterns/creation/AdapterTest.java
@@ -10,10 +10,15 @@ import org.junit.Test;
 public class AdapterTest {
 
     private String className;
+    private String variable1Type;
+    private String variable1Name;
 
     @Before
     public void setup() {
         className = "TestClass";
+
+        String variable1Type = "String";
+        String variable11Name = "name";
     }
 
     /**
@@ -38,5 +43,19 @@ public class AdapterTest {
         className = null;
         CodeBuilder codeBuilder2 = new CodeBuilder(className);
         codeBuilder2.build();
+    }
+
+    /**
+     * Test to verify the addField can accept and store a Class/Primitive and the variable name and allow the developer
+     * to chain functions
+     */
+    @Test
+    public void adapterTestAddField() {
+        String answer = String.format("public class %s { public %s %s; }", className, variable1Type, variable1Name) ;
+        CodeBuilder codeBuilder = new CodeBuilder(className);
+        String result = codeBuilder.build();
+
+        Assert.assertEquals(answer, result);
+
     }
 }
