c605d64d28e0316990ff78495da823bbfcf1cba0
Charles Stockman <chuck.stockman@yahoo.com>
2020-11-22 23:55:16 -0500

    Added test to instantiate class

---
+++ src/test/java/org/charlesStockman/designPatterns/creation/AdapterTest.java
@@ -0,0 +1,17 @@
+package org.charlesStockman.designPatterns.creation;
+
+import org.junit.Test;
+
+/**
+ * Test written to help the developer understand the adapter class
+ */
+public class AdapterTest {
+
+    /**
+     * Test to verify the instance of class CodeBuilder can be created
+     */
+    @Test
+    public void AdapterTestClass() {
+        CodeBuilder codeBuilder;
+    }
+}
